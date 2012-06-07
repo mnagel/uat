16286db2ce91e834f3f2b96473c8c9036b28b81a
markus <markus@1cde74c2-b0a7-11dd-9e77-cbfca2305f68>
2012-06-07 18:31:50 +0000

    sleeptime in testclient depends on params' distance to optimal values
    git-svn-id: https://svn.ipd.kit.edu/repos/karcher/karcher/markus/Tuner@2330 1cde74c2-b0a7-11dd-9e77-cbfca2305f68

--- Client/testClient.cpp
+++ Client/testClient.cpp
@@ -1,27 +1,35 @@
 #include <string>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "tuner.h"
+
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	int testVariable = 1337;
-	int second = 1338;
-	//int third = 1339;
-	//int fourth = 1340;
+	int variables[] = {1, 2, 3, 4};
+	int min[] = {1, 5, 1, 10};
+	int max[] = {20, 30, 10, 50};
+	int optimum[] = {10, 5, 3, 46};
+	int numberToUse = 3;
+
 	Tuner* myTuner = new Tuner();
 
 	string name = "testVariable";
 	const char* nameAsChar = name.c_str();
-	myTuner->tRegisterParameter(nameAsChar, &testVariable, 10, 20, 1); 
-	//myTuner->tRegisterParameter(nameAsChar, &fourth, 6, 8, 1); 
-	//myTuner->tRegisterParameter(nameAsChar, &third, 5, 7, 1); 
-	myTuner->tRegisterParameter(nameAsChar, &second, 1, 20, 1, TYPE_NUMBER_THREADS); 
+	for(int i=0; i<numberToUse; i++) {
+		myTuner->tRegisterParameter(nameAsChar, variables+i, min[i], max[i], 1); 
+	}
+
 	myTuner->tGetInitialValues();
-	for(int i=0; i<10; i++) {
+	for(int i=0; i<1000; i++) {
 		myTuner->tStart();
-		usleep(3000*1000);
+		int sleep = 0;
+		for(int j=0; j<numberToUse; j++) {
+			sleep += 100*abs(variables[j] - optimum[j]);
+		}
+		usleep(sleep*1000);
 		myTuner->tStop();
 	}
 	myTuner->tFinishTuning();
