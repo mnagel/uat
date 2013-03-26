adf12bee9a9be349b57d6e5a59e397781c1f7a75
Michael Nagel <michael.nagel@devzero.de>
2013-03-26 10:14:16 +0100

    from markus via mn: commit old changes to EvalNumIterations

--- Eval/EvalNumIterations/evalNumIterations.cpp
+++ Eval/EvalNumIterations/evalNumIterations.cpp
@@ -13,30 +13,31 @@ using namespace std;
 
 
 Tuner* myTuner;
-#define numParas 4
+#define numParas 6
 
 
-int variables[] = {10, 10, 10, 10};
-int minValue[] = {1, 1, 1, 1};
-int maxValue[] = {20, 20, 20, 20};
-int optimum[] = {1, 5, 	10, 15};
+int variables[] = {10, 10, 10, 10, 10, 10, 10, 10, 10};
+int minValue[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+int maxValue[] = {20, 20, 20, 20, 20, 20, 20, 20, 20};
+int optimum[] = {5, 15, 5, 15, 5, 15, 5, 15, 5};
 
 int main(int argc, char *argv[]) {
 
 	myTuner = new Tuner();
+	myTuner->tGetInitialValues();
 
 	for(int i=0; i<numParas; i++) {
 		myTuner->tRegisterParameter(variables+i, minValue[i], maxValue[i], 1);
 		myTuner->tRegisterSectionParameter(1, variables+i); 
 	}
 
-	for(int i=0; i<250; i++) {
+	for(int i=0; i<1000; i++) {
 		myTuner->tRequestStart(1);
 		int sleep = 100;
 		for(int j=0; j<numParas; j++) {
 			sleep += 50*abs(variables[j] - optimum[j]);
 		}
-		usleep(sleep*1000);
+		usleep(sleep*10);
 		myTuner->tStop(1);
 	}
 }
